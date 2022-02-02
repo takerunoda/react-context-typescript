5d1575ef1d37ee74de4bb94ed94babedc6718886
Takeru Noda <nodatakeru.business@gmail.com>
2022-02-02 13:45:23 +0900

    typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app'
 import { PageWrapper } from '../context/PageContext'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps } : AppProps) {
   return (
   <>
     <PageWrapper>
