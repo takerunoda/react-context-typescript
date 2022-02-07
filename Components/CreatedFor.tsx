fe04e4c6ab42c316d7489868639f829de125b1d2
Takeru Noda <nodatakeru.business@gmail.com>
2022-02-07 12:14:48 +0900

    CreatedFor

--- Components/CreatedFor.tsx
+++ Components/CreatedFor.tsx
@@ -1,13 +1,14 @@
 const CreatedFor = () => {
 const url = process.env.NEXT_PUBLIC_BLOG_URL
 const author = process.env.NEXT_PUBLIC_MY_NAME
+const twitter = process.env.NEXT_PUBLIC_MY_TWITTER
     return (
-        <div className="text-center w-72 mx-auto text-xs">
+        <div className="text-center w-72 mx-auto text-sm mt-8">
             <p>
-                This project was created for <a href={url} className="text-blue-500 hover:underline" target="_blank" rel="noreferrer">a blog post</a>
+                This project was created for <a href={url} className="text-blue-500 hover:underline cursor-pointer" target="_blank" rel="noreferrer">a blog post</a>
             </p>
             <p>
-                {`written by ${author}`}                
+                written by <span><a href={twitter} target="_blank" className="text-blue-500 hover:underline cursor-pointer" rel="noreferrer">{author}</a></span>
             </p>
         </div>
       
